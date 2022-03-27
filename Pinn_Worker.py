c4e3ec1b6af5c584fffa2ef016806cc5c561e775
Félix Fdez <felix.fdez.dlm@gmail.com>
2022-03-27 21:37:44 +0200

    Modified Evaluation Method
    Old Pinn Worker method considered predictions on every functional included in the System object. Now it does so by default, but a list of evaluation functionals can be passed, so that Pinn Worker only evaluates over those. Temporary fix (should be better structured).

--- Pinn_Worker.py
+++ Pinn_Worker.py
@@ -26,7 +26,7 @@ import time
 class PINN_Worker(Worker):
     
     def __init__(self,valData,test_gridObj,PDESystem,configspecs,valFromFEM=True,valParams={},
-                 experiment_name='DEFAULT',eval_mode='functional',
+                 experiment_name='DEFAULT',eval_mode='functional',eval_functionals=None,
                  *args,**kwargs):
         
         #Worker elements
@@ -61,21 +61,26 @@ class PINN_Worker(Worker):
                     values = dataFile[:,-1]
                 valuesSet.append(values)
                 pointsSet.append(points)
+        #Data can also be inputted as a full array. First columns in the array should be points, last ones should be values.
         else:
             for i in range(test_gridObj.ndim,valData.shape[1]):
                 pointsSet.append(valData[:,:test_gridObj.ndim])
                 valuesSet.append(valData[:,i])
+                
+        
         #Generar mallado y datos de test
         self.validation_preds = []
         print('Applying validation data to test grid')
         for values,points in zip(valuesSet,pointsSet):
             self.validation_preds.append(griddata(points, values, test_gridObj.grid , method='nearest'))
         self.test_gridObj = test_gridObj
+        
         #Guardar objeto System
         print('Saving PDE and config')
         self.PDESystem = PDESystem
         self.configspecs = configspecs
-        #
+        
+        
         if eval_mode == 'parameter':
             if len(valParams<1):
                 print('WARNING: Param mode was chosen, but no param values were given')
@@ -115,17 +120,33 @@ class PINN_Worker(Worker):
                                 batch_size=config['batch_size'],learning_rate=config['initial_lr'])        
         TrainTime = time.process_time()-start
         
-        preds = []
         start2 = time.process_time()
-        for functional in Functionals.values():
-            pred = functional.eval(m,[self.test_gridObj.grid[:,i] for i in range(self.test_gridObj.grid.shape[1])])
-            preds.append(pred)
+        if eval_functionals is None:
+            preds = []
+
+            for functional in Functionals.values():
+                pred = functional.eval(m,[self.test_gridObj.grid[:,i] for i in range(self.test_gridObj.grid.shape[1])])
+                preds.append(pred)
+        else:
+            preds = {}
+            for functional_name in eval_functionals:
+                preds[functional_name] = Functionals[functional_name].eval(m,[self.test_gridObj.grid[:,i] for i in range(self.test_gridObj.grid.shape[1])])
+                
+                
         TestTime = time.process_time() - start2
         
-        predErrors = []
-        for pred,val in zip(preds,self.validation_preds) :
-            predErrors.append(pred-val)
+        if eval_functionals is None:
+            predErrors = []
+            for pred,val in zip(preds,self.validation_preds) :
+                predErrors.append(pred-val)
+        else:
+            predErrors = []
+            for functional_name,valindex in zip(eval_functionals,range(len(eval_functionals)):
+                predErrors.append( preds[functional_name] - self.validation_preds[valindex])
+                
         
+        
+                                                
         L1 = self.test_gridObj.volume * np.sum([np.mean(np.abs(error)) for error in predErrors])
         
         L2 = self.test_gridObj.volume * np.sum([np.mean(error**2) for error in predErrors])
